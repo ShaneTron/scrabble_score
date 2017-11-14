417cec323f0506119c8f4ac2796bc38ffce6c51b
Malgorzata Haniszewska and Shane Robinson <shane@>
2017-11-14 15:20:29 -0800

    add specs for words that contains different letters

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,7 +1,24 @@
 class String
   def scrabble_score()
-    if self == "a"
-      1
+    array = self.split(//)
+    total = 0
+    array.each do |array_element|
+      if (array_element == "a") | (array_element == "e") | (array_element == "i") | (array_element == "o") | (array_element == "u") | (array_element == "l") | (array_element == "n") | (array_element == "r") | (array_element == "s") | (array_element == "t")
+        total += 1
+      elsif (array_element == "d") | (array_element == "g")
+        total += 2
+      elsif (array_element == "b") | (array_element == "c") | (array_element == "m") | (array_element == "p")
+        total += 3
+      elsif (array_element == "f") | (array_element == "h") | (array_element == "v") | (array_element == "w") |(array_element == "y")
+        total += 4
+      elsif (array_element == "k")
+        total += 5
+      elsif (array_element == "j") | (array_element == "x")
+        total += 8
+      elsif (array_element == "q") | (array_element == "z")
+        total += 10  
+      end
     end
+    return total
   end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,4 +6,10 @@ describe('scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble_score()).to(eq(1))
   end
+  it("returns a scrabble score for a word with two letters") do
+    expect("at".scrabble_score()).to(eq(2))
+  end
+  it("returns a scrabble score for a word that contains the letters from a different value scope") do
+    expect("adbfjkz".scrabble_score()).to(eq(33))
+  end
 end
